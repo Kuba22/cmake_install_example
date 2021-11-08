1b138680fbd00e450ed7b7e0f154aaa002a78a99
Kuba Nowak <jakub.nowak@forcom.com.pl>
2021-11-08 11:09:12 +0100

    With OpenCV dependency

--- mytestlib.cpp
+++ mytestlib.cpp
@@ -1,9 +1,8 @@
 #include "mytestlib.h"
 #include <iostream>
-// #include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 void MyTest::print(){
-    // cv::Mat m(1, 1, 0);
-    // std::cout<<"Printing from mytestlib "<<m.type()<<std::endl;
-    std::cout<<"Printing from mytestlib "<<std::endl;
+    cv::Mat m(1, 1, 0);
+    std::cout<<"Printing from mytestlib "<<m.type()<<std::endl;
 }
